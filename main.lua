19180b121f4f4e7a6e8311f0e8f3ec8e721dd599
orialdev <vitorgajogos@gmail.com>
2025-12-01 18:23:04 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/orialdev2/Vision-Hub/refs/heads/main/main.lua"))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/0d2f1f117e57f9aab7d6bbc6088c548be0184d9a11272f4a6007fe8a45ab398b/download"))()
