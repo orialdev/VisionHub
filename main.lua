c2970bee74f555d20c0c20d29fce323fca3dd156
orialdev <vitorgajogos@gmail.com>
2026-01-15 00:06:19 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/de9de68600e0987a65dce25891e81253dcc4f1f86fc03bfd299c5ec6a8f5320e/download"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/de9de68600e0987a65dce25891e81253dcc4f1f86fc03bfd299c5ec6a8f5320e/download"))()
