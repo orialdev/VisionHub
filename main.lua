d771c3f99684c3fb2dd92ea7ec88299bd00fca71
orialdev <vitorgajogos@gmail.com>
2026-02-19 19:03:56 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,3 @@
+getgenv().SCRIPT_KEY = "KEYLESS"
+
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/de9de68600e0987a65dce25891e81253dcc4f1f86fc03bfd299c5ec6a8f5320e/download"))()
